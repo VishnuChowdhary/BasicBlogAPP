0dc86152d893bf0b8ab7860bc916dfe3fdb0cdae
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-01-24 13:33:24 +0530

    List Template Created

--- BlogAPP/settings.py
+++ BlogAPP/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BlogAPP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Posts/views.py
+++ Posts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+# POST
+def post_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+# GET
+def post_detail(request):
+    return HttpResponse("<h1>Get Details</h1>")
+
+# GET
+def post_list(request):
+    queryset = Post.objects.all()
+
+    context = {
+        'object_list' :queryset,
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("<h1>Display List</h1>")
+
+# UPDATE
+def post_update(request):
+    return HttpResponse("<h1>Update Data</h1>")
+
+# DELETE
+def post_delete(request):
+    return HttpResponse("<h1>Delete Data</h1>")
