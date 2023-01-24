51c536d1684b3b70595f61494f4c51f89e0afb4e
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-01-24 12:03:54 +0530

    Post APP Model file created

--- Posts/admin.py
+++ Posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'updated', 'timestamp']
+    list_filter = ['updated']
+    search_fields = ['title']
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
